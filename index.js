5e1949c2f1ebf30a46cd6e96d6af6238099bcb36
Shumei Huang <rose1264@gmail.com>
2018-05-10 18:20:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ var servedCustomer = 0;
 
 function takeANumber(line) {
   line.push(`${servedCustomer + 1}`);
-  console.log(`Welcome, you are number ${line.length} in line.`) ;
+  (`Welcome, you are number ${line.length} in line.`);
 }
 
 function nowServing(line) {
   if (!line.length) {
-    return "There is nobody waiting to be served!";
+    console.log("There is nobody waiting to be served!");
   }
   servedCustomer++;
-  return `Currently serving ${servedCustomer + 1}.`;
+  console.log(`Currently serving ${servedCustomer + 1}.`);
 }
 
 function currentLine(line) {
   if (!line.length || (line.length === servedCustomer + 1)) {
-    return "The line is currently empty.";
+    console.log("The line is currently empty.");
   }
-  return `The line is currently: ${line.length}`;
+  console.log(`The line is currently: ${line.length}`);
 }
 
 //test
